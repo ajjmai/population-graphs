73980e0e661d4d9d7ace0b1b2be25e2b595e2d8d
Aija <aija.valleala@gmail.com>
2020-04-10 20:07:01 +0300

    add fetch population data functionality

--- myChart.js
+++ myChart.js
@@ -2,6 +2,24 @@ function logCountryCode() {
     var countryCode = document.getElementById("country").value;
     console.log(countryCode);
 }
+
+// EventListener for the Button
 document
     .getElementById("renderButton")
-    .addEventListener("click", logCountryCode);
+    .addEventListener("click", fetchData);
+
+// fetch data from the World Bank API
+async function fetchData() {
+    var countryCode = document.getElementById("country").value;
+    const indicatorCode = "SP.POP.TOTL";
+    const baseUrl = "https://api.worldbank.org/v2/country/";
+    const url = baseUrl + countryCode + /indicator/ + indicatorCode + "?format=json";
+    console.log("Fetching data from URL: " + url);
+
+    var response = await fetch(url);
+
+    if (response.status == 200) {
+        var fetchedData = await response.json();
+        console.log(fetchedData)
+    }
+}
